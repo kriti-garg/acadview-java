d305a35a696e3dd02a7486ddb5cd1b4a7029bd5b
kriti-garg <kritigarg1998@gmail.com>
2018-06-21 16:52:01 +0530

    final code

--- assignment11/Project.java
+++ assignment11/Project.java
@@ -13,27 +13,29 @@ public class Project {
 	            listf(file.getAbsolutePath(), files,fileName);
 	        }
 	    }
-	    //System.out.println("1 2 3");
 	}
    public static void main(String[] args) throws IOException{
-	   Scanner sc = new Scanner(System.in);
+	  /* Scanner sc = new Scanner(System.in);
 	   String str1,str2;
 	   System.out.println("enter the path where you want to search the files");
 	   str1 = sc.nextLine();
 	   System.out.println("enter the path where you want to store the content");
-	   str2 = sc.nextLine();
-	   System.out.println(str1);
+	   str2 = sc.nextLine();*/
+       File f1 = new File("//home//kriti//Desktop//java_assignments//assignment11//abc.txt");
+   	   FileReader fin = new FileReader(f1);
+       BufferedReader bin = new BufferedReader(fin);
+       String str1,str2;
+       str1 = bin.readLine();
+       str2= bin.readLine();
+       System.out.println(str1);
        System.out.println(str2);
-	   
-	    List<File> list = new ArrayList<File>();
+       List<File> list = new ArrayList<File>();
 	    List<String> listFile = new ArrayList<String>();
 	    listf(str1,list,listFile);
 	    int i = list.size();
-	    
 	    File outputFile = new File(str2+"/Project.csv");
 		outputFile.createNewFile();
-	   
-	    FileWriter inputFile = new FileWriter(outputFile);
+	   FileWriter inputFile = new FileWriter(outputFile);
 	    inputFile.write("FILENAME , FILEPATH\n");
 	    for(int j=0;j<i;j++) {
 	    	 inputFile.write(listFile.get(j));
