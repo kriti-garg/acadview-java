1b5c4d06f7aa80a5c05970eaaa820cedada436ef
kriti-garg <kritigarg1998@gmail.com>
2018-06-28 00:26:30 +0530

    intial commit  in assign17

---
+++ assignment17/Login/app/src/main/java/LoginActivity.java
@@ -0,0 +1,12 @@
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.kriti.login.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
