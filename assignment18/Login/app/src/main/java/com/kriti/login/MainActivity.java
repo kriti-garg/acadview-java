55b6ab6a1902fa445fdeb78ed87051e879b8ca27
kriti-garg <kritigarg1998@gmail.com>
2018-06-28 18:35:19 +0530

    added constraints on MainActivity

--- assignment18/Login/app/src/main/java/com/kriti/login/MainActivity.java
+++ assignment18/Login/app/src/main/java/com/kriti/login/MainActivity.java
@@ -2,60 +2,74 @@ package com.kriti.login;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
-/*
-    EditText name = findViewById(R.id.Name);
-    EditText email = findViewById(R.id.E_mail);
-    EditText password = findViewById(R.id.password);*/
+   private String et_name,et_mail,et_phone,et_password,c_password;
+    boolean valid =true;
+   private EditText name,email,phone,password,cpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        name = (EditText) findViewById(R.id.et_name);
+        email = findViewById(R.id.et_mail);
+        phone = findViewById(R.id.et_phone);
+        password = findViewById(R.id.et_password);
+        cpassword = findViewById(R.id.c_password);
         Button button = findViewById(R.id.register);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(), "registered", Toast.LENGTH_SHORT).show();
+                checkDataEntered();
             }
-        });
+        });}
+        public void intialise(){
+            et_name = name.getText().toString().trim();
+            et_mail = email.getText().toString().trim();
+            et_phone = phone.getText().toString().trim();
+            et_password = password.getText().toString().trim();
+            c_password = cpassword.getText().toString().trim();
+        }
+   public void checkDataEntered() {
+        intialise();
+        if(!validate()){
+             Toast.makeText(this,"Signup has failed",Toast.LENGTH_SHORT).show();
+        }
+        else
+            onSignUpSuccess();
     }
+    public boolean validate(){
+        valid =true;
+            if(et_name.isEmpty()||name.length()>32){
+                name.setError("Please enter valid name");
+                valid=false;
+            }
+            if(et_mail.isEmpty() ||!Patterns.EMAIL_ADDRESS.matcher(et_mail).matches()){
+                email.setError("Please enter valid email address");
+                valid = false;
+            }
+            /*if(password == cpassword){
+                valid = true;
+            }
+            else {
+                password.setError("passwords are not same");
+                valid = false;
+            }*/
+            return valid;
+    }
+    public void onSignUpSuccess(){
+          if(valid == true )
+          {
+              Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+              startActivity(intent);
+              Toast.makeText(getApplicationContext(), "registered", Toast.LENGTH_SHORT).show();
+          }
+    }
+
 }
-
-   /* boolean isEmail(EditText text) {
-        CharSequence email = text.getText().toString();
-        return (!TextUtils.isEmpty(email)) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
-
-    boolean isEmpty(EditText text) {
-        CharSequence str = text.getText().toString();
-        return (TextUtils.isEmpty(str));
-    }
-
-    void checkDataEntered() {
-        if (isEmpty(name)) {
-            Toast.makeText(this, "You must enter name to register!!", Toast.LENGTH_SHORT).show();
-        }
-        if (isEmpty(phone)) {
-            phone.setError("address is required");
-        }
-        if (isEmail(email) == false) {
-            email.setError("enter a valid email id");
-        }
-        if (isEmpty(password)){
-            password.setError("password cannot be empty");
-        }
-        if(password == repassword) {
-        }
-        else password.setError("both not same");
-        }*/
-
-
 
