46bdf70648ac076aec303b1df02835960a584317
kriti-garg <kritigarg1998@gmail.com>
2018-06-29 09:42:07 +0530

    added data folder

--- assignment19/Login/app/src/main/java/com/kriti/login/data/RegContract.java
+++ assignment19/Login/app/src/main/java/com/kriti/login/data/RegContract.java
@@ -1,8 +1,18 @@
 package com.kriti.login.data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by kriti on 29/6/18.
  */
 
-public class RegContract {
+final class RegContract {
+    private RegContract(){ }
+    public final static class RegEntry implements BaseColumns{
+        public final static String TABLE_NAME ="registration_details";
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_USERNAME ="username";
+        public final static String COLUMN_EMAIL = "email";
+        public final static String COLUMN_PASSWORD = "password";
+    }
 }
--- assignment19/Login/app/src/main/java/com/kriti/login/data/RegDbHelper.java
+++ assignment19/Login/app/src/main/java/com/kriti/login/data/RegDbHelper.java
@@ -1,8 +1,66 @@
 package com.kriti.login.data;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.kriti.login.data.RegContract.RegEntry;
 /**
  * Created by kriti on 29/6/18.
  */
 
-public class RegDbHelper {
+public class RegDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "registration_details.db";
+    private static final int DATABASE_VERSION = 1;
+    public RegDbHelper(Context context){
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db){
+        String SQL_CREATE_REG_DETAILS_TABLE = "CREATE_TABLE " + RegEntry.TABLE_NAME + "("
+                + RegEntry._ID + "  INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +RegEntry.COLUMN_EMAIL + "  TEXT NOT NULL,"
+                +RegEntry.COLUMN_USERNAME + "TEXT NOT NULL,"
+                +RegEntry.COLUMN_PASSWORD + "TEXT NOT NULL);";
+        Log.v("RegDbHelper" , "create table: " + SQL_CREATE_REG_DETAILS_TABLE);
+        db.execSQL(SQL_CREATE_REG_DETAILS_TABLE);
+
+    }
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){ }
+    public Cursor readDetails(){
+        SQLiteDatabase db = getReadableDatabase();
+        String[] projection = {
+                RegEntry._ID,
+                RegEntry.COLUMN_EMAIL,
+                RegEntry.COLUMN_USERNAME,
+                RegEntry.COLUMN_PASSWORD
+        };
+        Cursor cursor =db.query(
+                RegEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        return  cursor;
+    }
+    public void insertDetails(String emailString , String usernameString , String passwordString){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(RegContract.RegEntry.COLUMN_EMAIL,emailString);
+        values.put(RegContract.RegEntry.COLUMN_USERNAME,emailString);
+        values.put(RegContract.RegEntry.COLUMN_PASSWORD,emailString);
+        long newRowId = db.insert(RegContract.RegEntry.TABLE_NAME,null,values);
+        if(newRowId==-1){
+            Log.v("RegDbHelper", "Error in insertion");
+        }
+        else{
+            Log.v("RegDbHelper","insertion Successful");
+        }
+    }
+
 }
