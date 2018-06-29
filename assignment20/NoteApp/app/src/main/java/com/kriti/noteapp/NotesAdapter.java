6d01dbbcc4629e33c980f96ce9288811dacb927b
kriti-garg <kritigarg1998@gmail.com>
2018-06-30 01:46:17 +0530

    add database

---
+++ assignment20/NoteApp/app/src/main/java/com/kriti/noteapp/NotesAdapter.java
@@ -0,0 +1,42 @@
+package com.kriti.noteapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+import com.kriti.noteapp.NoteContract.NoteEntry;
+
+
+/**
+ * Created by kriti on 30/6/18.
+ */
+
+public class NotesAdapter extends CursorAdapter {
+    private final MainActivity activity;
+    public NotesAdapter(MainActivity context){
+        super(context, null, 0 /* flags */);
+        this.activity = context;
+    }
+    public void bindView(View view, Context context, Cursor cursor){
+        TextView titleTextView = view.findViewById(R.id.titleText);
+        TextView noteTextView = view.findViewById(R.id.noteText);
+       int titleColumnIndex= cursor.getColumnIndex(NoteEntry.COLUMN_TITLE);
+        int noteColumnIndex=cursor.getColumnIndex(NoteEntry.COLUMN_NOTE);
+
+        String title = cursor.getString(titleColumnIndex);
+        String note = cursor.getString(noteColumnIndex);
+
+        titleTextView.setText(title);
+        noteTextView.setText(note);
+
+
+    }
+    public View newView(Context context, Cursor cursor, ViewGroup parent){
+        return LayoutInflater.from(context).inflate(R.layout.list, parent, false);
+    }
+}
